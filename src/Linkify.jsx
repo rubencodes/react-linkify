5f664caab7666af2ddd40a05909135d7d63e139a
Ruben Martinez Jr <ruben.martinez93@gmail.com>
2017-05-01 14:09:12 -0700

    New pattern that doesn't crash (important).

--- src/Linkify.jsx
+++ src/Linkify.jsx
@@ -5,8 +5,7 @@ function isEmail(email) {
   return re.test(email);
 }
 function findMatches(text) {
-  const urlRegex = '(?!mailto:)(?:(?:http|https|ftp)://)?(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?';
-  const pattern = new RegExp(urlRegex, 'ig');
+  const pattern = /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig;
 
   const matches = [];
   let match;
